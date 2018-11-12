05e650475d43d4cb761b38399de93c69c11e8501
Li <Shutai.Li@sandc.com>
2018-11-12 16:40:56 -0500

    Updates

---
+++ precache-manifest.d13fb5d4cd934724a1335f53f3e87554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d65eeecbc6dae33d2a8a",
+    "url": "/my-page/static/js/runtime~main.d65eeecb.js"
+  },
+  {
+    "revision": "cb74cd8d4d90059783fd",
+    "url": "/my-page/static/js/main.cb74cd8d.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/my-page/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "cb74cd8d4d90059783fd",
+    "url": "/my-page/static/css/main.7400a907.chunk.css"
+  },
+  {
+    "revision": "67c756383b20f02b918c86a4cf98cacf",
+    "url": "/my-page/index.html"
+  }
+];
